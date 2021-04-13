f495980b5d9cb92d06dc6de7cdb14c8ff329b7f0
Geo Thomas <geothomas@cet.ac.in>
2021-04-13 10:01:05 +0530

    app_name and app_developer

--- playstore_dataset/playstore_dataset/items.py
+++ playstore_dataset/playstore_dataset/items.py
@@ -3,4 +3,8 @@ import scrapy
 
 
 class PlaystoreDatasetItem(scrapy.Item):
+    # define the fields for your item here like:
+    app_name = scrapy.Field()
+    app_developer = scrapy.Field()
+
     pass
--- playstore_dataset/playstore_dataset/spiders/dataset.py
+++ playstore_dataset/playstore_dataset/spiders/dataset.py
@@ -1,9 +1,17 @@
 import scrapy
+from ..items import PlaystoreDatasetItem
+
 
 
 class DatasetSpider(scrapy.Spider):
     name = 'dataset'
-    start_urls = ['http://https://play.google.com/store/']
+    start_urls = ['https://play.google.com/store/apps/collection/cluster?clp=SjEKKwolcHJvbW90aW9uXzMwMDIxMDNfZ292dHNlcnZpY2VzX2FwcHNpbhBKGAM6AggB:S:ANO1ljKg9wc&gsr=CjNKMQorCiVwcm9tb3Rpb25fMzAwMjEwM19nb3Z0c2VydmljZXNfYXBwc2luEEoYAzoCCAE%3D:S:ANO1ljKl0oc']
 
     def parse(self, response):
-        pass
+        apps=PlaystoreDatasetItem()
+        app_name=response.css('.WsMG1c.nnK0zc::text').extract()
+        app_developer=response.css('.KoLSrc::text').extract()
+        apps['app_name']=app_name
+        apps['app_developer']=app_developer
+        
+        yield apps
