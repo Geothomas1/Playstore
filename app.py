3ac6d328216906581fa2ed92e329a24ad16c8cee
Geo Thomas <geothomas@cet.ac.in>
2021-05-19 07:49:34 +0530

    code refactor

---
+++ app.py
@@ -0,0 +1,8 @@
+from google_play_scraper import app
+
+result = app(
+    'com.nianticlabs.pokemongo',
+    lang='en', # defaults to 'en'
+    country='us' # defaults to 'us'
+)
+print(result['title'])
