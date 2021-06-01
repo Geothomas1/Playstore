5e8651af4eb5fbd1ab2cbc4c5785f82af68c7db7
Geo Thomas <geothomas@cet.ac.in>
2021-06-01 14:17:38 +0530

    From developer? url success

---
+++ app_id_developer_id_url/app_id_collection_developer_id_url/settings.py
@@ -0,0 +1,22 @@
+
+BOT_NAME = 'app_id_collection_developer_id_url'
+SPIDER_MODULES = ['app_id_collection_developer_id_url.spiders']
+NEWSPIDER_MODULE = 'app_id_collection_developer_id_url.spiders'
+ROBOTSTXT_OBEY = False
+SPLASH_URL = 'http://localhost:8050/'
+PROXY_POOL_ENABLED = True
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+
+}
+SPIDER_MIDDLEWARES = {
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+}
+# HTTPCACHE_ENABLED = True
+# HTTPCACHE_EXPIRATION_SECS = 0
+# HTTPCACHE_DIR = 'httpcache'
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
---
+++ app_id_developer_id_url/app_id_collection_developer_id_url/spiders/developer.py
@@ -0,0 +1,44 @@
+import scrapy
+from scrapy_splash import SplashRequest
+
+main_list=[]
+script = """
+                    function main(splash,args)
+                        splash:set_viewport_size(1028, 10000)
+                        splash:go(args.url)
+                        local scroll_to = splash:jsfunc("window.scrollTo")
+                        scroll_to(0, 2900)
+                        splash:wait(8)
+                        return {
+                            html = splash:html()
+                        }
+                    end
+                  """
+
+#url=['https://play.google.com/store/apps/developer?id='+i for i in tag]
+class DeveloperSpider(scrapy.Spider):
+    name = 'developer'
+    allowed_domains = ['x']
+    def start_requests(self):
+        tag=['put id']
+        for i in tag:
+            url='https://play.google.com/store/apps/developer?id='+i
+            yield SplashRequest(url, self.parse,  endpoint='execute', args={'lua_source': script, 'url': url})
+        
+
+    def parse(self, response):
+        app_id_list=[]
+        app_id=response.css('.b8cIId.ReQCgd.Q9MA7b a::attr(href)').extract() 
+        for link in app_id:
+            app_id_list.append(link.split('=')[-1])
+        for x in app_id_list:
+            main_list.append(x)
+        #print(main_list)
+        temp_list = []
+        for i in main_list:
+            if i not in temp_list:
+                temp_list.append(i)
+        print(len(temp_list))
+        print(temp_list)  
+
+        
